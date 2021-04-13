6968a228cb39d945abb930a9729a1fcb042abbeb
alextar04 <taranlesha@mail.ru>
2021-04-13 13:05:09 +0300

    connected Tests Module, installed Hippolyte, make scheme for tests module

---
+++ BSTU App/BSTU App Tests/Mocks/MockServer.swift
@@ -0,0 +1,15 @@
+//
+//  MockServer.swift
+//  BSTU App
+//
+//  Created by Alexey Taran on 13.04.2021.
+//  Copyright © 2021 Alexey Taran. All rights reserved.
+//
+
+import Foundation
+import Hippolyte
+
+
+class MockServer{
+    
+}
---
+++ BSTU App/BSTU App Tests/ScheduleTests.swift
@@ -0,0 +1,19 @@
+//
+//  ScheduleTests.swift
+//  BSTU App Tests
+//
+//  Created by Alexey Taran on 13.04.2021.
+//  Copyright © 2021 Alexey Taran. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import BSTU_App
+
+class ScheduleTests: XCTestCase{
+    
+    func testOne(){
+        XCTAssertEqual(1, 1)
+    }
+    
+}
